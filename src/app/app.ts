8ec5419add7cab04d940845a9d899d1cd5e43c15
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-01-28 10:39:43 -0600

    intial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NuevoComponente} from './nuevo-componente/nuevo-componente';
+import {ComponenteInline} from './componente-inline/componente-inline';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NuevoComponente, ComponenteInline],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app-angular');
+  protected readonly title = signal('Hola Mundo desde Angular Js');
+  protected readonly name = "Alan Altamirano Hernández";
 }
